466b4717684875880e407b551675034f931eb45f
Nodirbek <nbek.3d@gmail.com>
2023-06-15 13:36:23 +0500

    IF|Nullish|Switch

---
+++ 5/homework.js
@@ -0,0 +1,201 @@
+//10
+
+// let n=2,sum=0;
+
+// for(let i=1;i<=n;i++){
+//     sum+=1/i;
+// }
+
+// console.log(sum);
+
+//11
+
+// let n=2,sum=0;
+
+// for(let i=n;i<=2*n;i++){
+//     sum+=i*i;
+// }
+
+// console.log(sum);
+
+//12
+
+// let n=2,sum=1,x=1.1;
+
+// for(let i=1;i<=n;i++){
+//     sum*=x;
+//     x+=0.1
+// }
+
+// console.log(sum);
+
+//13
+
+// let n=2,sum=0,x=1.1;
+
+// for(let i=1;i<=n;i++){
+//     sum+=x;
+//     x+=0.1;
+//     x*=-1;
+// }
+
+// console.log(sum);
+
+//14
+
+// let n=2,sum=0;
+
+// for(let i=1;i<=n;i++){
+//     sum+=2*i-1;
+// }
+
+// console.log(sum);
+
+
+//15
+
+// let  n=3,a=4,sum=1;
+
+// for(let i=1;i<=n;i++){
+//     sum*=a
+// }
+
+// console.log(sum);
+
+//16
+
+// let  n=3,a=4,sum=1;
+
+// for(let i=1;i<=n;i++){
+//     sum*=a;
+//     console.log(sum);
+// }
+
+//17
+
+// let  n=2,a=4,sum=1,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=a;
+//     sum+=res;
+// }
+
+
+// console.log(sum);
+
+//18
+
+// let  n=2,a=4,sum=1,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=-a;
+//     sum+=res;
+// }
+
+
+// console.log(sum);
+
+//19
+
+// let  n=5,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=i;
+// }
+
+
+// console.log(res);
+
+//20
+
+// let  n=2,res=1,sum=0,nis;
+
+// for(let i=1;i<=n;i++){
+//     res*=i;
+//     sum+=res;
+// }
+
+
+// console.log(sum);
+
+//21
+
+// let  n=2,res=1,sum=0,nis;
+
+// for(let i=1;i<=n;i++){
+//     res*=i;
+//     nis=1/res;
+//     sum+=nis;
+// }
+
+
+// console.log(sum);
+
+//22
+
+// let sum=1,x=3,n=2,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=x/i;
+//     sum+=res;
+// }
+
+// console.log(sum);
+
+//23
+
+// let sum=0,x=3,n=3,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=x/i;
+//     if(i%2!=0){
+//         res*=-1;
+//         sum+=res;
+//     }
+// }
+
+// console.log(-sum);
+
+
+//24
+
+// let sum=1,x=3,n=3,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=x/i;
+//     if(i%2==0){
+//         res*=-1;
+//         sum+=res;
+//     }
+// }
+
+// console.log(sum);
+
+//25
+
+// let sum=1,x=3,n=3,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=x;
+//     if(i%2==0){
+//         res*=-1;
+//         sum+=res/i;
+//     }
+// }
+
+// console.log(sum);
+
+//26 ma
+
+// let sum=0,x=3,n=3,res=1;
+
+// for(let i=1;i<=n;i++){
+//     res*=x;
+//     if(i%2!==0){
+//         res*=-1;
+//         sum+=res/i;
+//     }
+// }
+
+// console.log(-sum);
+
